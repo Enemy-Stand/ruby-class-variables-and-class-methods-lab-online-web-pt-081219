3236d54e6a109f8b9561a3f033f0069c1faadb4c
Joseph Sesti <sestijoey63@gmail.com>
2019-09-09 20:54:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@song_count = 0
-  
-  def initialize
-    @@song_count += 1
-  end
-  
-  def self.song_count
-    @@song_count
-  end
-  
+  @@song_artists = []
+  @@song_genres = []
   
 end
