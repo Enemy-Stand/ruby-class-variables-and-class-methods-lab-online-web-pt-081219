c6ef3e54726b501e77728fc8edec4b1a35163abc
Joseph Sesti <sestijoey63@gmail.com>
2019-09-09 21:58:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,19 @@ class Song
   artist_count
   end
   
-  
   def self.genre
     @@genres.uniq
   end
+  
+    def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[artist]
+        artist_count =+ 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+  artist_count
   
 end
